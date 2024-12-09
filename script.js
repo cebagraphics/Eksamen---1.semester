a7fe3ee59f96cd3c4db34667106d98af868dc24e
cebagraphics <ceciliebang03@gmail.com>
2024-12-09 18:44:08 +0100

    Samlet 1 CSS fil
    Jeg har samlet alle CSS-filerne jeg havde i én i stedet.

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ document.querySelector('.portrætter').addEventListener('click', function() {
     document.getElementById('image1').classList.toggle('active');
     document.getElementById('image2').classList.toggle('active');
 });
+
