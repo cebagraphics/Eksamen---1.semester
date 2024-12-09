51993811a46ddb8bd3086c3afd57c57be23ce58a
cebagraphics <ceciliebang03@gmail.com>
2024-12-09 18:38:00 +0100

    Start
    Min første commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.portrætter').addEventListener('click', function() {
+    document.getElementById('image1').classList.toggle('active');
+    document.getElementById('image2').classList.toggle('active');
+});
