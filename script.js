22e49afc211889bae511b2915b0cff795004c0b3
cebagraphics <ceciliebang03@gmail.com>
2024-12-11 17:12:59 +0100

    Nye undersider og små ændringer
    Jeg har lavet nye undersider til hvert projekt og små ændringer generelt i de eksisterende sider.

--- script.js
+++ script.js
@@ -2,4 +2,3 @@ document.querySelector('.portrætter').addEventListener('click', function() {
     document.getElementById('image1').classList.toggle('active');
     document.getElementById('image2').classList.toggle('active');
 });
-
